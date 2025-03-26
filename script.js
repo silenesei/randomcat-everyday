038dbf66a529251ef14f82f6a9dc93475c3c48e3
silenesei <marcelina.klimczewska@gmail.com>
2025-03-27 00:33:36 +0100

    final version

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const $getCatBtn = document.getElementById("get-btn");
 const $idCat = document.getElementById("cat");
 const $clickSound = document.getElementById("click-sound");
 const $loadingInd = document.getElementById("loading-ind");
+const $result = document.getElementById("result");
 
 $getCatBtn.addEventListener("click", () => {
   $clickSound.play();
@@ -10,11 +11,19 @@ $getCatBtn.addEventListener("click", () => {
 $getCatBtn.addEventListener("click", async () => {
   $loadingInd.style.display = "block";
   $idCat.style.visibility = "hidden";
+  $result.classList.remove("show");
 
   const response = await fetch("https://api.thecatapi.com/v1/images/search");
   const data = await response.json();
 
   $idCat.src = data[0].url;
-  $idCat.style.visibility = "visible";
-  $loadingInd.style.display = "none";
+
+  $idCat.onload = () => {
+    $idCat.style.visibility = "visible";
+    $loadingInd.style.display = "none";
+
+    setTimeout(() => {
+      $result.classList.add("show");
+    }, 1000);
+  };
 });
